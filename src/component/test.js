6933ae92e996deec40ead51230bd7921807be089
AlexanderIII <oniichann183@gmail.com>
2025-01-06 14:28:50 +0700

    react-hook

---
+++ src/component/test.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
+const test = () => {
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <div>Count = {count}</div>
+                <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+                <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+            </header>
+        </div>
+    );
+}
+
+export default test;
