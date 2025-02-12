0025a796a06335c7965d283a9cc0f7023b92edf7
AlexanderIII <oniichann183@gmail.com>
2025-02-12 15:53:57 +0700

    AlexNguyen

--- src/component/MyComponent.js
+++ src/component/MyComponent.js
@@ -1,33 +1,72 @@
 import React from "react";
-class MyComponent extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: ''
+import ListUser from "./ListUser";
+import { useState } from 'react';
 
-        }
-    }
-    handleChange = (event, id) => {
-        let copyState = []
-        copyState = { ...this.state }
-        copyState[id] = event.target.value
-        this.setState({
-            ...copyState
-        })
+
+const MyComponent = () => {
+    const [name, setName] = useState('');
+    const [appear, setAppear] = useState(false);
+    const [list, setList] = useState([
+        { name: 'Anh', age: '25' },
+        { name: 'Alex', age: '25' },
+        { name: 'Adam', age: '23' },
+
+    ])
+
+    const handleChange = (event, id) => {
+        let copyState = '';
+        copyState = event.target.value
+        setName(copyState)
     };
+    const handleHover = (event) => {
 
+        setAppear(!appear)
 
-
-    render() {
-        console.log('check state name', this.state.name)
-        return (
-            <>
-                <h1> Heloo mother fucker : {this.state.name}</h1>
-                <input onChange={(event) => { this.handleChange(event, 'name') }}></input>
-            </>
-
-        )
     }
+    return (
+        <>
+            <h1> Heloo mother fucker : {name}</h1>
+            <input
 
+
+                onChange={(event) => { handleChange(event) }}></input>
+            <button onMouseOver={handleHover}>click tao</button>
+            {appear === true ? <div>the anh dep trao</div> : ''
+            }
+
+            <hr></hr>
+            <ListUser listUser={list} />
+        </>
+
+    )
 };
+// class MyComponent extends React.Component {
+//     // constructor(props) {
+//     //     super(props);
+//     //     this.state = {
+//     //         name: '',
+//     //         appear: false,
+//     //         list: [
+//     //             { name: 'Anh', age: '25' },
+//     //             { name: 'Alex', age: '25' },
+//     //             { name: 'Adam', age: '23' },
+
+//     //         ]
+
+//     //     }
+//     // }
+//     // componentDidMount = () => {
+//     //     // change tile when loading another page
+//     //     document.title = "The Anh"
+//     // }
+
+
+
+
+//     render() {
+//         console.log('check state name', this.state.name)
+
+//     }
+
+// };
 export default MyComponent;
