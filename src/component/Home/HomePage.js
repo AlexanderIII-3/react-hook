c411c2c4d0454f53cdbc7d1a22ee74edfde75140
AlexanderIII <oniichann183@gmail.com>
2025-02-13 16:03:38 +0700

    css

--- src/component/Home/HomePage.js
+++ src/component/Home/HomePage.js
@@ -3,10 +3,18 @@ import './HomePage.scss';
 const HomePage = () => {
     return (
         <div className="homepage-container">
-            holo
             <video autoPlay muted loop  >
                 <source src={videoHomePage} type="video/mp4" />
             </video>
+            <div className='homepage-content'>
+                <div className='homepage-title' >There's the best way to learn </div>
+                <div className='homepage-description'>We are here to assistent you improving your self , do let it better yesterday! </div>
+                <div className='btn-infor'>
+                    <button>Click here if you are interested
+                    </button>
+
+                </div>
+            </div>
 
         </div>
     )
